a6c79a6996db981b0f4333523ddd33557d92c5b4
MUHDPROject™ <71631017+MUHDPROject@users.noreply.github.com>
2023-01-20 11:21:09 +0700

    Add files via upload

---
+++ Absensi-Apps/app/src/main/java/com/kel4/absensi/viewmodel/HistoryViewModel.kt
@@ -0,0 +1,29 @@
+package com.kel4.absensi.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.kel4.absensi.database.DatabaseClient.Companion.getInstance
+import com.kel4.absensi.database.dao.DatabaseDao
+import com.kel4.absensi.model.ModelDatabase
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.schedulers.Schedulers
+
+class HistoryViewModel(application: Application) : AndroidViewModel(application) {
+    var dataLaporan: LiveData<List<ModelDatabase>>
+    var databaseDao: DatabaseDao? = getInstance(application)?.appDatabase?.databaseDao()
+
+    fun deleteDataById(uid: Int) {
+        Completable.fromAction {
+            databaseDao?.deleteHistoryById(uid)
+        }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe()
+    }
+
+    init {
+        dataLaporan = databaseDao!!.getAllHistory()
+    }
+}
